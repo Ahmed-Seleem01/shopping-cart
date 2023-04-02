55b98b002f210252f6c87e30f269669e116e79d5
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-02 21:51:52 +0200

    Add Homepage component

---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import "../styles/homepage.scss";
+
+const Homepage = () => {
+  return (
+  <div className="homepage">
+    <div className="description">
+      <h2>Ingrediento</h2>
+      <p>Great taste starts here.</p>
+      <button>Shop Now</button>
+    </div>
+  </div>
+  )
+};
+
+export default Homepage;
