f8ed78bd3c06e529102dd7297eb5713447675bd5
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-07 13:39:23 +0200

    Update the routes make new routes and let different components to communicate and share data between each other between card and cart components to update the items in the cart

--- src/components/SwitchRoutes.js
+++ src/components/SwitchRoutes.js
@@ -1,19 +1,24 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter,HashRouter, Routes, Route } from "react-router-dom";
 import Card from "./Card";
+import Cart from "./Cart";
 import Header from "./Header";
 import Homepage from "./Homepage";
 import Shopping from "./Shopping";
+import "normalize.css";
+import { useState } from "react";
 
 const SwitchRoutes = () => {
+  const [headerCart, setHeaderCart] = useState([]);
   return (
-    <BrowserRouter>
-      <Header />
+    <HashRouter>
+      <Header  headerCart = {headerCart}/>
+      {/* <Cart /> */}
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/shopping" element={<Shopping />} />
-        <Route path="/shopping/:id" element={<Card/>}/>
+        <Route path="/shopping/:name" element={<Card setHeaderCart = {setHeaderCart}/>}/>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
