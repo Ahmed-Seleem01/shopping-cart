d75bb77fdeb0a82c0a5ca75c0f967119c21f3538
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-07 13:35:27 +0200

    Update to fetch the API and render ingredients

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/shopping.scss";
+
 const Shopping = () => {
-  return(
-    <div>Shopping</div>
-  )
+  const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch("https://ahmed-seleem01.github.io/test-cart/ingredientsAPI.json", {
+      mode: 'cors'
+    })
+      .then((response) => response.json())
+      .then((data) => setIngredients(data.ingredients))
+      .catch((error) => console.error(error));
+  }, []);
+
+  // console.log(ingredients);
+  return (
+    <div className="ingredients-cards">
+      {ingredients.map((ingredient) => (
+        <Link to={`/shopping/${ingredient.name}`}>
+          <div key={ingredient.id} className="ingredient-card">
+            <h2>{ingredient.name}</h2>
+            <img src={ingredient.image} alt={ingredient.name} />
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Shopping;
