7f2679331894dc9a2ac61e2be24cd830a1c03114
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-15 22:07:25 +0200

    Update the logic of handle the cart items Make them separate items with adjusting the quantity for each item in the cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import "../styles/cart.scss";
+import "../styles/card.scss";
+import SetQuantity from "./SetQuantity";
+
 const Cart = ({ hide, showCart, headerCart }) => {
   const [items, setItems] = useState([]);
 
@@ -13,18 +16,23 @@ const Cart = ({ hide, showCart, headerCart }) => {
     }
   }, [headerCart])
 
+  const handleQuantityChange = (index, newQuantity) => {
+    const updatedItems = [...items];
+    updatedItems[index][0] = newQuantity;
+    setItems(updatedItems);
+  }
+
   return (
     <div className={showCart}>
       <button className="close-cart" onClick={closePopup}>X</button>
-      {items.map((item) => (
-        <div>
+      {items.map((item, index) => (
+        <div key={index} className="cart-item">
           <span>{item[2]}</span>
-          <span>{item[0]}</span>
-          {/* <span>{item[1]}$</span> */}
+          <span><SetQuantity quantity={item[0]} setQuantity={(newQuantity) => handleQuantityChange(index, newQuantity)} /></span>
           <span>{(item[1] * item[0]).toFixed(2)}$</span>
           <button>Checkout</button>
         </div>
-      ))} 
+      ))}
     </div>
   );
 };
