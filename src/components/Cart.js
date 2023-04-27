60926aed0399c73bde94bdad678d1f9c577d64b3
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-27 19:03:54 +0200

    Add comments and refactor the logic of the state

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,38 +1,100 @@
-import { useEffect, useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+// Import the required modules and components
+import { useEffect } from "react";
 import "../styles/cart.scss";
 import "../styles/card.scss";
 import SetQuantity from "./SetQuantity";
 
-const Cart = ({ hide, showCart, headerCart }) => {
-  const [items, setItems] = useState([]);
-
-  const closePopup = ()=>{
+// Define the Cart component
+//  This component takes the following props: hide,
+// showCart, headerCart, setCartItems, items, and setItems.
+const Cart = ({
+  hide,
+  showCart,
+  headerCart,
+  setCartItems,
+  items,
+  setItems,
+}) => {
+  // Define a function to close the popup
+  const closePopup = () => {
     hide();
-  }
+  };
 
-  useEffect(()=>{
-    if(headerCart.length>0){
-      setItems([...items, headerCart])
+  // Define an effect hook that updates the cart items when the header cart changes
+  /*
+    This effect runs whenever headerCart changes. If headerCart is not empty 
+    and the cart already contains an item with the same name, the quantity of that 
+    item is increased. Otherwise, the new item is added to the cart. The
+    setCartItems function is also called to update the number of items in the cart.
+   */
+  useEffect(() => {
+    if (headerCart.id) {
+      if (items.some((item) => item.name === headerCart.name)) {
+        const arr = items.map((item) => {
+          if (item.name === headerCart.name) {
+            item.quantity += headerCart.quantity;
+          }
+          return item;
+        });
+        setItems(arr);
+      } else {
+        setItems([...items, headerCart]);
+      }
+      setCartItems(items.length);
     }
-  }, [headerCart])
+  }, [headerCart]);
 
+  // Define a function to handle quantity changes
   const handleQuantityChange = (index, newQuantity) => {
     const updatedItems = [...items];
-    updatedItems[index][0] = newQuantity;
+    updatedItems[index].quantity = newQuantity;
     setItems(updatedItems);
-  }
+  };
 
+  // Define a function to delete an item from the cart
+  const deleteItem = (e) => {
+    console.log(e.target.id);
+    const updatedItems = items
+      .slice(0, e.target.id)
+      .concat(items.slice(Number(e.target.id) + 1));
+    setItems(updatedItems);
+    setCartItems(updatedItems.length);
+  };
+  // Return the cart UI
+  /*
+    This component displays the cart items, their quantity, and their total price.
+    It also provides a way to update the quantity or delete the item. 
+    If there are no items in the cart, a message is displayed instead.
+  */
   return (
     <div className={showCart}>
-      <button className="close-cart" onClick={closePopup}>X</button>
-      {items.length > 0 ? items.map((item, index) => (
-        <div key={index} className="cart-item">
-          <span>{item[2]}</span>
-          <span><SetQuantity quantity={item[0]} setQuantity={(newQuantity) => handleQuantityChange(index, newQuantity)} /></span>
-          <span>{(item[1] * item[0]).toFixed(2)}$</span>
-          <button>Checkout</button>
-        </div>
-      )) : "No items"}
+      <button className="close-cart" onClick={closePopup}>
+        X
+      </button>
+      {items.length > 0
+        ? items.map((item, index) => (
+            <div key={index} className="cart-item">
+              <span>{item.name}</span>
+              <img src={item.image} alt={item.name} />
+              <span>
+                <SetQuantity
+                  quantity={item.quantity}
+                  setQuantity={(newQuantity) =>
+                    handleQuantityChange(index, newQuantity)
+                  }
+                />
+              </span>
+              <span>{(item.quantity * item.price).toFixed(2)}$</span>
+              <div className="cart-buttons">
+                <button>Checkout</button>
+                <button id={index} onClick={deleteItem}>
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))
+        : "No items"}
     </div>
   );
 };
