e6708bfbb43633aba74e47958f822e9719050895
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-27 16:04:27 +0200

    Add comments and some style modification

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,15 +1,24 @@
-import "../styles/homepage.scss";
+// Import the 'Link' component from the 'react-router-dom' library and the 'homepage.scss' stylesheet
 import { Link } from "react-router-dom";
+import "../styles/homepage.scss";
+
+// Define a functional component called 'Homepage'
 const Homepage = () => {
   return (
+    // Render a div element with the class name 'homepage'
     <div className="homepage">
+      {/* Render a div element with the class name 'description' to hold information */}
       <div className="description">
         <h2>Ingrediento</h2>
         <p>Great taste starts here.</p>
-        <button><Link to="/shopping">Shop Now</Link></button>
+        {/* Render a Link component that points to the '/shopping' route, when the button is clicked */}
+        <Link to="/shopping">
+          <button>Shop Now</button>
+        </Link>
       </div>
     </div>
   );
 };
 
+// Export the 'Homepage' component as a default export
 export default Homepage;
