d783e59cd1d04a413446c0ad7330dbcaf87e155c
Ahmed-Seleem01 <ahmedseleem.c@gmail.com>
2023-04-27 20:44:00 +0200

    Add some comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,52 +1,65 @@
+// Importing necessary dependencies
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import SetQuantity from "./SetQuantity";
 import "../styles/card.scss";
 
-const Card = ({setHeaderCart}) => {
+// Card component definition
+const Card = ({ setHeaderCart }) => {
+  // State variables for the ingredient and its quantity
   const [ingredient, setIngredient] = useState({});
   const [quantity, setQuantity] = useState(1);
+
+  // Extracting the ingredient name from the URL parameters
   const ingredientName = useParams().name;
 
-  // const handleChange = (e)=>{
-  //   console.log("e.target")
-  //   setQuantity(5)
-  // }
-;
-  useEffect(()=>{
-    setQuantity(quantity)
-  },[quantity])
-
-  const handleSubmit = ()=>{
-    setHeaderCart([quantity, ingredient.price, ingredient.name]);
-    setQuantity(1);
-  }
-
-  
+  // useEffect hook to update the quantity state variable
   useEffect(() => {
-    fetch('https://ahmed-seleem01.github.io/shopping-cart/ingredientsAPI.json', {
-      mode: 'cors'
-    })
-      .then(response => response.json())
-      .then(data => {
-        const selectedIngredient = data.ingredients.find(ingredient => ingredient.name === ingredientName);
+    setQuantity(quantity);
+  }, [quantity]);
+
+  // Function to add the selected ingredient to the header cart
+  const handleSubmit = () => {
+    ingredient.quantity = quantity;
+    const updatedObj = Object.assign({}, ingredient);
+    setHeaderCart(updatedObj);
+    setQuantity(1);
+  };
+
+  // useEffect hook to fetch data from the ingredients API and update the ingredient state variable
+  useEffect(() => {
+    fetch(
+      "https://ahmed-seleem01.github.io/shopping-cart/ingredientsAPI.json",
+      {
+        mode: "cors",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const selectedIngredient = data.ingredients.find(
+          (ingredient) => ingredient.name === ingredientName
+        );
         setIngredient(selectedIngredient);
       })
-      .catch(error => console.error(error));
+      .catch((error) => console.error(error));
   }, [ingredientName]);
 
+  // Card component rendering
   return (
-    <div className = "card">
+    <div className="card">
       <h2>{ingredient.name}</h2>
       <img src={ingredient.image} alt={ingredient.name} />
       <p>{ingredient.description}</p>
       <p>price: ${ingredient.price}</p>
       <div>
-        <SetQuantity quantity={quantity} setQuantity={setQuantity}/>
-        <button className="add-btn" onClick={handleSubmit} >Add to cart</button>
+        <SetQuantity quantity={quantity} setQuantity={setQuantity} />
+        <button className="add-btn" onClick={handleSubmit}>
+          Add to cart
+        </button>
       </div>
     </div>
   );
-}
+};
 
+// Exporting the Card component as the default module
 export default Card;
